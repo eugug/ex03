0e958f09ee706134aea60f97f2e92ae3ac0c0580
eugug <142820616+eugug@users.noreply.github.com>
2023-09-01 10:45:00 -0400

    Add files via upload

---
+++ exercicio03.js
@@ -0,0 +1,25 @@
+//módulo 1//
+
+ const Guga = {
+        nome:'Pita',
+        raca:'vir-lata+pitbull' 
+        idade:7,
+        latir:function(){
+            console.log('brinca muito');
+        }
+ }
+ zeus.latir();
+
+ //módulo//
+ let vetor = [10,20,30,40,50,60,70,80,90,100];
+ vetor[vetor.length - 1] = undefined;
+ console.log(vetor);
+
+ //módulo 3//
+ var frutas = new array("morango", "goiaba", "maçã", "melancia", "acerola");
+ console.log(frutas.join("-"));
+
+ //módulo 4//
+
+ var times = new Array("corinthians", "flamengo", "são paulo");
+ console.log(tipyof times);
